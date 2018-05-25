8a03d72ca8588aff02deaf6314e7be71448118cd
Kin Torres <kin.mello@gmail.com>
2018-05-25 02:41:38 -0300

    Adicionado validação de formulario com diretiva ngIf

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   onSubmit(form){
-    console.log(form);
+    console.log(form.value);
   }
   constructor() { }
 
